d1ce456d6c27a4ecf4e4d02385d1251823d19724
Derrick James <ballyouth@gmail.com>
2022-07-16 09:17:40 -0400

    local host established

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
+const path = require('path');
+const fs = require ('fs');
 const express = require('express');
-const app = express();
-const notes = require ('./db/db.json')
+const { notes } = require ('./db/db.json'); 
+
+
 const PORT = 3000;
+const app = express();
+
+app.listen(3000, () => {console.log(`Server running on http://localhost:${PORT}`)});
