3bb0dc08a78e5c0163ae2b053fb85ab268215773
Derrick James <ballyouth@gmail.com>
2022-07-16 13:57:24 -0400

    found value data

--- server.js
+++ server.js
@@ -12,13 +12,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 //HTML 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(_dirname + '/public/index.html'))});
+    res.sendFile(path.join(__dirname, '/public/index.html'))});
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(_dirname + '/public/notes.html'))});
+    res.sendFile(path.join(__dirname, '/public/notes.html'))});
 app.get('/api/notes', (req, res) => {
-    fs.readFile('./db/db.json', 'utf8', (err,data) => {
+    fs.readFile('./db/db.json', "utf8", (err,data) => {
+        const note = JSON.parse(data);
         if (err) throw err;
-        console.log(data);
+        //console.log(data);
+        res.json(note);
     })
 });
+app.post('/api/notes', (req, res) => {
+    console.log(req.body);
+})
 app.listen(PORT, () => {console.log(`Server running on http://localhost:${PORT}`)});
