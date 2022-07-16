a4da1eb881edece394bd0a125412729526b26f92
Derrick James <ballyouth@gmail.com>
2022-07-16 12:58:37 -0400

    server gets notes

--- server.js
+++ server.js
@@ -10,8 +10,15 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-
+//HTML 
 app.get('/', (req, res) => {
     res.sendFile(path.join(_dirname + '/public/index.html'))});
-app.get('/api/notes', (req, res) => {res.json(notes)});
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(_dirname + '/public/notes.html'))});
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err,data) => {
+        if (err) throw err;
+        console.log(data);
+    })
+});
 app.listen(PORT, () => {console.log(`Server running on http://localhost:${PORT}`)});
