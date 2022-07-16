722726acccf88246b2d6ceb83db6d83ba52ecb88
Derrick James <ballyouth@gmail.com>
2022-07-16 11:25:50 -0400

    connected index and notes

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const path = require('path');
-const fs = require ('fs');
+const fs = require('fs');
 const express = require('express');
-const { notes } = require ('./db/db.json'); 
+const notes = require('./db/db.json'); 
 
 
 const PORT = 3000;
 const app = express();
 
-app.listen(3000, () => {console.log(`Server running on http://localhost:${PORT}`)});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(_dirname + '/public/index.html'))});
+app.get('/api/notes', (req, res) => {res.json(notes)});
+app.listen(PORT, () => {console.log(`Server running on http://localhost:${PORT}`)});
